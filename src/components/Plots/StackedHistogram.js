8a2b29de6d0426181ad0b4b002eccb51c6f39ed3
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2021-12-23 14:51:05 -0800

    fix, refactor: using a consistent color scheme, fixing headers, heights etc so that things can fit into a single row and are consistent across all gallary plots (#11)

--- src/components/Plots/StackedHistogram.js
+++ src/components/Plots/StackedHistogram.js
@@ -16,7 +16,7 @@ const StackedHistogram = (props) => {
         }
 
         if (!color) {
-            color = "#69b3a2";
+            color = "#00B3A4";
         }
 
         if (!data) return "";
