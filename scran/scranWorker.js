a10c4fa38d8a2d6ec48faa198087ed284f773bba
jkanche <jkanche@users.noreply.github.com>
2021-12-28 22:04:23 +0000

    Deploying to gh-pages from @ jkanche/scran.js.app@1c9dfc15e9d7450c30acb671fda959054296592b 🚀

--- scran/scranWorker.js
+++ scran/scranWorker.js
@@ -583,7 +583,7 @@ function runAllSteps(wasm, state) {
     });
   }
 
-  var pca_out = utils.processOutput(runPCA, wasm, { "num_hvgs": 4000, "num_pcs": state.params.pca["pca-npc"] });
+  var pca_out = utils.processOutput(runPCA, wasm, { "num_hvgs": state.params.pca["pca-hvg"], "num_pcs": state.params.pca["pca-npc"] });
   if (norm_out !== null) {
     postMessage({
       type: `${pca_out["$step"]}_DATA`,
@@ -639,7 +639,7 @@ function runAllSteps(wasm, state) {
     });
   }
 
-  var choose_out = utils.processOutput(chooseClustering, wasm, { "method": "snn_graph" });
+  var choose_out = utils.processOutput(chooseClustering, wasm, { "method": state.params.cluster["clus-method"] });
   if (choose_out !== null) {
     postMessage({
       type: `${choose_out["$step"]}_DATA`,
