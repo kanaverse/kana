5fb6e8229fc25299e3cb6fefe283440baa77d7f1
LTLA <LTLA@users.noreply.github.com>
2022-01-04 05:36:42 +0000

    Deploying to gh-pages from @ jkanche/kana@02a6085177f45ab8f52c3aee02656151a674975a 🚀

--- scran/_inputs.js
+++ scran/_inputs.js
@@ -8,7 +8,7 @@ const scran_inputs = {};
   /** Public members **/
   x.changed = false;
 
-  /** Private functions **/
+  /** Private functions (misc) **/
   function mockFiles(files) {
     var mock = [];
     for (const f of files) {
@@ -41,6 +41,51 @@ const scran_inputs = {};
     return genes;
   }
 
+  /** Private functions (MatrixMarket) **/
+  function loadMatrixMarketRaw(wasm, getMtxBuffer, getGenesBuffer, getBarcodesBuffer) {
+    var contents = new Uint8Array(getMtxBuffer());
+    var files = { "type": "MatrixMarket", "buffered": {} };
+    files.buffered.mtx = contents;
+
+    scran_utils.freeCache(cache.matrix);
+    var buffer = new WasmBuffer(wasm, contents.length, "Uint8Array");
+    try {
+      buffer.set(contents);
+      var ext = mtx_files[0].name.split('.').pop();
+      var is_compressed = (ext == "gz");
+      cache.matrix = wasm.read_matrix_market(buffer.ptr, mtx_files[0].size, is_compressed);
+    } finally {
+      buffer.free();
+    }
+
+    /** TODO: support Gzipped TSV files here. Also support loading from a buffer object. **/
+//    const tsv = d3.dsvFormat("\t");
+//
+//    if (barcode_file.length > 0) {
+//      var reader = new FileReaderSync();
+//      var file_size = barcode_file[0].size;
+//      var buffer = reader.readAsText(barcode_file[0]);
+//      cache.barcodes = tsv.parse(buffer);
+//    }
+//
+//    if (genes_file.length > 0) {
+//      var reader = new FileReaderSync();
+//      var file_size = genes_file[0].size;
+//      var buffer = reader.readAsText(genes_file[0]);
+//      cache.gene_names = tsv.parse(buffer); 
+//    } else {
+//      cache.gene_names = dummyGenes(cache.matrix.nrow());
+//    }
+
+    cache.gene_names = dummyGenes(cache.matrix.nrow());
+
+    cache.gene_names = permuteGenes(wasm, cache.gene_names);
+
+    cache.files = files;
+    delete cache.reloaded; 
+    return;
+  }
+
   function loadMatrixMarket(wasm, input) {
     var mtx_files = input[0];
     var barcode_file = input[1];
@@ -57,44 +102,42 @@ const scran_inputs = {};
         return;
     }
 
-    scran_utils.freeCache(cache.matrix);
-    var reader = new FileReaderSync();
-    var file_size = mtx_files[0].size;
-    var contents = reader.readAsArrayBuffer(mtx_files[0]);
-    var contents = new Uint8Array(contents);
-
-    var buffer = new WasmBuffer(wasm, file_size, "Uint8Array");
-    try {
-      buffer.set(contents);
-      var ext = mtx_files[0].name.split('.').pop();
-      var is_compressed = (ext == "gz");
-      cache.matrix = wasm.read_matrix_market(buffer.ptr, mtx_files[0].size, is_compressed);
-    } finally {
-      buffer.free();
-    }
-
-    /** TODO: support Gzipped TSV files here. **/
-    const tsv = d3.dsvFormat("\t");
-
-    if (barcode_file.length > 0) {
-      var reader = new FileReaderSync();
-      var file_size = barcode_file[0].size;
-      var buffer = reader.readAsText(barcode_file[0]);
-      cache.barcodes = tsv.parse(buffer);
-    }
-
-    if (genes_file.length > 0) {
-      var reader = new FileReaderSync();
-      var file_size = genes_file[0].size;
-      var buffer = reader.readAsText(genes_file[0]);
-      cache.gene_names = tsv.parse(buffer); 
-    } else {
-      cache.gene_names = dummyGenes(cache.matrix.nrow());
-    }
+    loadMatrixMarketRaw(wasm,
+      () => {
+        var reader = new FileReaderSync();
+        return reader.readAsArrayBuffer(mtx_files[0]);
+      },
+      () => {},
+      () => {}
+    );
 
     parameters = mock_args;
-    delete cache.reloaded; 
     x.changed = true;
+    return;
+  }
+
+  /** Private functions (HDF5) **/
+  function loadHDF5Raw(wasm, getHDF5Buffer) {
+    scran_utils.freeCache(cache.matrix);
+
+    var contents = getHDF5Buffer();
+    cache.matrix = readMatrixFromHDF5(wasm, contents); 
+    var files = { "type": "HDF5", "buffered": { "h5": contents } };
+
+    var genes = guessGenesFromHDF5(contents);
+    if (genes === null) {
+      cache.gene_names = dummyGenes(cache.matrix.nrow());
+    } else {
+      /** TODO: handle multiple names properly. **/
+      for (const [key, val] of Object.entries(genes)) {
+        cache.gene_names = val;
+        break;
+      }
+    }
+    cache.gene_names = permuteGenes(wasm, cache.gene_names);
+
+    cache.files = files;
+    delete cache.reloaded; 
     return;
   }
 
@@ -109,24 +152,12 @@ const scran_inputs = {};
         return;
     }
 
-    scran_utils.freeCache(cache.matrix);
-    var reader = new FileReaderSync();
-    var contents = reader.readAsArrayBuffer(h5_files[0]);
-    cache.matrix = readMatrixFromHDF5(wasm, contents); 
-
-    var genes = guessGenesFromHDF5(contents);
-    if (genes === null) {
-      cache.gene_names = dummyGenes(cache.matrix.nrow());
-    } else {
-      /** TODO: handle multiple names properly. **/
-      for (const [key, val] of Object.entries(genes)) {
-        cache.gene_names = val;
-        break;
-      }
-    }
+    loadHDF5Raw(wasm, () => {
+      var reader = new FileReaderSync();
+      return reader.readAsArrayBuffer(h5_files[0]);
+    });
 
     parameters = mock_args;
-    delete cache.reloaded; 
     x.changed = true;
     return;
   }
@@ -143,7 +174,6 @@ const scran_inputs = {};
       throw "unknown matrix file extension for '" + first_name + "'";
     }
    
-    cache.gene_names = permuteGenes(wasm, cache.gene_names);
     return;
   };
 
@@ -159,15 +189,18 @@ const scran_inputs = {};
     var contents = {};
 
     if ("reloaded" in cache) {
-      contents = contents.reloaded;
+      contents.gene_names = contents.reloaded.gene_names;
+      contents.num_cells = contents.reloaded.num_cells;
+      contents.files = contents.reloaded.files;
     } else {
       contents.gene_names = cache.gene_names;
       contents.num_cells = cache.matrix.ncol();
+      contents.files = cache.files;
     }
 
     return {
       "parameters": parameters,
-      "contents": x.results(wasm)        
+      "contents": contents
     };
   };
 
@@ -180,7 +213,17 @@ const scran_inputs = {};
   /** Public functions (custom) **/
   x.fetchCountMatrix = function(wasm) {
     if ("reloaded" in cache) {
-      /** TODO: something to reconstitute the matrix! **/
+      if (cache.reloaded.files.type == "MatrixMarket") {
+        loadMatrixMarketRaw(wasm, 
+          () => cache.reloaded.files.buffered.mtx,
+          () => cache.reloaded.files.buffered.barcodes,
+          () => cache.reloaded.files.buffered.genes 
+        );
+      } else {
+        loadHDF5Raw(wasm, 
+          () => cache.reloaded.files.buffered.h5
+        );
+      }
     }
     return cache.matrix;
   };
