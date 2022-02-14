7d51644bedaab87e04a27765c90453a58e3f3824
Kancherla <jayaram.kancherla@gmail.com>
2022-02-13 19:10:58 -0800

    restore loaded params, fix mismatch with umap params

--- src/workers/scran.worker.js
+++ src/workers/scran.worker.js
@@ -271,9 +271,9 @@ function runAllSteps(mode = "run", state = null) {
             } else {
                 umap.unserialize(state[step]);
                 addParameters("umap", {
-                    "num_epochs": state[step].parameters.num_epochs,
-                    "num_neighbors": state[step].parameters.num_neighbors,
-                    "min_dist": state[step].parameters.min_dist,
+                    "umap-epochs": state[step].parameters.num_epochs,
+                    "umap-nn": state[step].parameters.num_neighbors,
+                    "umap-min_dist": state[step].parameters.min_dist,
                     "animate": state[step].parameters.animate
                 });
             }
