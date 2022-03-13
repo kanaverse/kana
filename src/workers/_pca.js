713c46eadcd5f108a755e196cc5ec0721a9adb6a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-03-12 18:09:32 -0800

    Flipped the PCA step.

--- src/workers/_pca.js
+++ src/workers/_pca.js
@@ -10,35 +10,55 @@ var parameters = {};
 export var changed = false;
 
 function fetchPCsAsWasmArray() {
-    if ("reloaded" in cache) {
+    if (!("pcs" in cache)) {
         return cache.reloaded.pcs;
     } else {
-        return cache.raw.principalComponents({ copy: "view" });
+        return cache.pcs.principalComponents({ copy: "view" });
     }
 }
 
-export function compute(args) {
-    if (!normalization.changed && !variance.changed && !utils.changedParameters(parameters, args)) {
-        changed = false;
-    } else {
-        // Choosing the highly variable genes.
-        var sorted_resids = variance.fetchSortedResiduals();
-        var threshold_at = sorted_resids[sorted_resids.length - args.num_hvgs];
+function chooseFeatures(num_hvgs) {
+    var sorted_resids = variance.fetchSortedResiduals();
+    var threshold_at = sorted_resids[sorted_resids.length - num_hvgs];
+    var sub = utils.allocateCachedArray(sorted_resids.length, "Uint8Array", cache, "hvg_buffer");
+    var unsorted_resids = variance.fetchResiduals({ unsafe: true });
+    sub.array().forEach((element, index, array) => {
+        array[index] = unsorted_resids[index] >= threshold_at;
+    });
+}
+
+export function compute(num_hvgs, num_pcs) {
+    changed = false;
+    
+    if (variance.changed || num_hvgs !== parameters.num_hvgs) {
+        chooseFeatures(num_hvgs);
+        parameters.num_hvgs = num_hvgs;
+        changed = true;
+    }
+
+    if (changed || normalization.changed || num_pcs !== parameters.num_pcs) { 
+        // Need to check this in case we never ran a chooseFeatures
+        // call, e.g., because we're running from a reloaded analysis
+        // where the user only changed the number of PCs.
+        if (!("hvg_buffer" in cache)) {
+            chooseFeatures(parameters.num_hvgs);
+        }
+        let sub = cache.hvg_buffer;
 
         var mat = normalization.fetchNormalizedMatrix();
-        var sub = utils.allocateCachedArray(mat.numberOfRows(), "Uint8Array", cache);
-        var unsorted_resids = variance.fetchResiduals({ unsafe: true });
-        sub.array().forEach((element, index, array) => {
-            array[index] = unsorted_resids[index] >= threshold_at;
-        });
+        utils.freeCache(cache.pcs);
+        cache.pcs = scran.runPCA(mat, { features: sub, numberOfPCs: args.num_pcs });
 
-        // Actually performing the PCA.
-        utils.freeCache(cache.raw);
-        cache.raw = scran.runPCA(mat, { features: sub, numberOfPCs: args.num_pcs });
-
-        utils.freeReloaded(cache);
+        parameters.num_pcs = num_pcs;
         changed = true;
-        parameters = args;
+    }
+
+    if (changed) {
+        // Free some memory.
+        if (reloaded !== null) {
+            utils.free(reloaded.pcs);
+            reloaded = null;
+        }
     }
     return;
 }
@@ -46,10 +66,10 @@ export function compute(args) {
 export function results() {
     var var_exp;
 
-    if ("reloaded" in cache) {
-        var_exp = cache.reloaded.var_exp.slice();
+    if (!("pcs" in cache)) {
+        var_exp = reloaded.var_exp.slice();
     } else {
-        var pca_output = cache.raw;
+        var pca_output = cache.pcs;
         var_exp = pca_output.varianceExplained();
         var total_var = pca_output.totalVariance();
         var_exp.forEach((x, i) => {
@@ -60,24 +80,50 @@ export function results() {
     return { "var_exp": var_exp };
 }
 
-export function serialize() {
-    var to_save = results();
-    to_save.pcs = fetchPCsAsWasmArray().slice();
-    return {
-      "parameters": parameters,
-      "contents": to_save
-    };
+export function serialize(path) {
+    let fhandle = new scran.H5File(path);
+    let ghandle = fhandle.createGroup("pca");
+
+    {
+        let phandle = ghandle.createGroup("parameters"); 
+        phandle.writeDataSet("num_hvgs", "Int32", [], parameters.num_hvgs);
+        phandle.writeDataSet("num_pcs", "Int32", [], parameters.num_pcs);
+    }
+
+    {
+        let rhandle = ghandle.createGroup("results");
+
+        let ve = results().var_exp;
+        rhandle.writeDataSet("var_exp", "Float64", [ve.length], ve);
+
+        let pcs = fetchPCsAsWasmArray();
+        rhandle.writeDataSet("pcs", "Float64", [pcs.length], pcs);
+    }
 }
  
-export function unserialize(saved) {
-    parameters = saved.parameters;
+export function unserialize(path) {
+    let fhandle = new scran.H5File(path);
+    let ghandle = fhandle.openGroup("pca");
 
-    utils.freeReloaded(cache);
-    cache.reloaded = saved.contents;
+    {
+        let phandle = ghandle.openGroup("parameters"); 
+        parameters = { 
+            num_hvgs: phandle.openDataSet("num_hvgs", { load: true }).values[0],
+            num_pcs: phandle.openDataSet("num_pcs", { load: true }).values[0]
+        };
+    }
 
-    var tmp = scran.createFloat64WasmArray(cache.reloaded.pcs.length);
-    tmp.set(cache.reloaded.pcs);
-    cache.reloaded.pcs = tmp;
+    {
+        let rhandle = ghandle.openGroup("results");
+        reloaded = {
+            var_exp: rhandle.openDataSet("var_exp", { load: true }).values,
+        };
+
+        let pcs = rhandle.openDataSet("pcs", { load: true }).values
+        utils.allocateCachedArray(pcs.length, "Uint8Array", reloaded, "pcs");
+        reloaded.pcs.set(pcs);        
+    }
+
     return;
 }
 
