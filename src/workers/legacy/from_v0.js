a20f519c16da58138ed72e98aca63bdb10e34ac8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-03-17 21:53:21 -0700

    Updated the converter script.

--- src/workers/legacy/from_v0.js
+++ src/workers/legacy/from_v0.js
@@ -1,10 +1,5 @@
 import * as scran from "scran.js";
 
-function quick_write_string(handle, name, val) {
-    let xhandle = handle.createDataSet(name, "String", [], { maxStringLength: val.length });
-    xhandle.write(val);
-}
-
 function recoverTypedArrays(object) {
     if (Array.isArray(object)) {
         for (var i = 0; i < object.length; i++) {
@@ -69,39 +64,34 @@ export function convertFromVersion0(state, newfile) {
         let ghandle = fhandle.createGroup("inputs");
         let params = state.inputs.parameters;
         let phandle = ghandle.createGroup("parameters");
-        quick_write_string(phandle, "format", params.type);
+        phandle.writeDataSet("format", "String", [], params.type);
 
         let fihandle = phandle.createGroup("files")
         for (const [index, info] of params.files.entries()) {
             let xhandle = fihandle.createGroup(String(index));
-            quick_write_string(xhandle, "type", info.type);
-            quick_write_string(xhandle, "name", info.name);
+            xhandle.writeDataSet("type", "String", [], info.type);
+            xhandle.writeDataSet("name", "String", [], info.name);
 
             if (info.buffer instanceof Object) { // i.e., embedded
-                let ihandle = xhandle.createDataSet("offset", "Uint32", []);
-                ihandle.write(info.buffer.offset);
-                let ihandle2 = xhandle.createDataSet("size", "Uint32", []);
-                ihandle2.write(info.buffer.size);
+                xhandle.writeDataSet("offset", "Uint32", [], info.buffer.offset);
+                xhandle.writeDataSet("size", "Uint32", [], info.buffer.size);
             } else { // i.e. KanaDB links.
-                let ihandle = xhandle.createDataSet("id", "String", []);
-                ihandle.write(info.buffer);
+                xhandle.writeDataSet("id", "String", [], info.buffer);
             }
         }
 
         // Only storing the number of cells and genes. If we want the 
         // barcode annotations, we might as well just read from the source.
         let chandle = ghandle.createGroup("results");
-        let dset = chandle.createDataSet("dimensions", "Int32", [2]);
         let contents = state.inputs.contents;
         let ngenes = Object.values(contents.genes)[0].length;
-        dset.write([ngenes, contents.num_cells]);
+        chandle.writeDataSet("dimensions", "Int32", null, [ngenes, contents.num_cells]);
 
         // Unfortunately the v0 didn't contain enough information to 
         // easily reproduce the permutations, so we just save the already-permuted genes here.
         let gehandle = chandle.createGroup("genes");
         for (const [key, val] of Object.entries(contents.genes)) {
-            let dhandle = gehandle.createDataSet(key, "String", [val.length]);
-            dhandle.write(val);
+            gehandle.writeDataSet(key, "String", null, val);
         }
     }
 
@@ -112,47 +102,30 @@ export function convertFromVersion0(state, newfile) {
         let phandle = ghandle.createGroup("parameters");
 
         let mparams = state.quality_control_metrics.parameters;
-        {
-            let ihandle = phandle.createDataSet("use_mito_default", "Uint8", []);
-            ihandle.write(Number(mparams.use_mito_default));
-        }
-        quick_write_string(phandle, "mito_prefix", mparams.mito_prefix);
+        phandle.writeDataSet("use_mito_default", "Uint8", [], Number(mparams.use_mito_default));
+        phandle.writeDataSet("mito_prefix", "String", [], mparams.mito_prefix);
 
         let tparams = state.quality_control_thresholds.parameters;
-        {
-            let ihandle = phandle.createDataSet("nmads", "Float64", []);
-            ihandle.write(tparams.nmads);
-        }
+        phandle.writeDataSet("nmads", "Float64", [], tparams.nmads);
 
         // Saving all the contents.
         let chandle = ghandle.createGroup("results");
 
         let mhandle = chandle.createGroup("metrics");
         let mcontents = recoverTypedArrays(state.quality_control_metrics.contents);
-        {
-            let dhandle = mhandle.createDataSet("sums", "Float64", [mcontents.sums.length]);
-            dhandle.write(mcontents.sums);
-        }
-        {
-            let dhandle = mhandle.createDataSet("detected", "Int32", [mcontents.detected.length]);
-            dhandle.write(mcontents.detected);
-        }
-        {
-            let dhandle = mhandle.createDataSet("proportion", "Float64", [mcontents.proportion.length]);
-            dhandle.write(mcontents.proportion);
-        }
+        mhandle.writeDataSet("sums", "Float64", null, mcontents.sums);
+        mhandle.writeDataSet("detected", "Int32", null, mcontents.detected);
+        mhandle.writeDataSet("proportion", "Float64", null, mcontents.proportion);
 
         // Converting the thresholds into arrays to handle multi-batch analyses.
         let thandle = chandle.createGroup("thresholds");
         let tcontents = recoverTypedArrays(state.quality_control_thresholds.contents);
         for (const x of [ "sums", "detected", "proportion" ]) {
-            let dhandle = thandle.createDataSet(x, "Float64", [1]);
-            dhandle.write([tcontents[x]]);
+            thandle.writeDataSet(x, "Float64", null, [tcontents[x]]);
         }
 
         let disc = tcontents.discards;
-        let dhandle = chandle.createDataSet("discards", "Uint8", [disc.length]);
-        dhandle.write(disc);
+        chandle.writeDataSet("discards", "Uint8", null, disc);
 
         // Don't bother saving 'retained', we'll get that from 'discards'.
     }
@@ -169,17 +142,12 @@ export function convertFromVersion0(state, newfile) {
         let ghandle = fhandle.createGroup("feature_selection");
 
         let phandle = ghandle.createGroup("parameters");
-        {
-            let dhandle = phandle.createDataSet("span", "Float64", []);
-            dhandle.write(state.feature_selection.parameters.span);
-        }
+        phandle.writeDataSet("span", "Float64", [], state.feature_selection.parameters.span);
 
         let chandle = ghandle.createGroup("results");
         let contents = recoverTypedArrays(state.feature_selection.contents);
         for (const x of [ "means", "vars", "fitted", "resids" ]) {
-            let y = contents[x];
-            let dhandle = chandle.createDataSet(x, "Float64", [y.length]);
-            dhandle.write(y);
+            chandle.writeDataSet(x, "Float64", null, contents[x]);
         }
     }
 
@@ -190,22 +158,19 @@ export function convertFromVersion0(state, newfile) {
         let phandle = ghandle.createGroup("parameters");
         let params = state.pca.parameters;
         for (const x of [ "num_hvgs", "num_pcs" ]) {
-            let dhandle = phandle.createDataSet(x, "Int32", []);
-            dhandle.write(params[x]);
+            phandle.writeDataSet(x, "Int32", null, params[x]);
         }
 
         let chandle = ghandle.createGroup("results");
         let contents = recoverTypedArrays(state.pca.contents);
 
         let ve = contents.var_exp;
-        let vhandle = chandle.createDataSet("var_exp", "Float64", [ve.length]);
-        vhandle.write(ve);
+        chandle.writeDataSet("var_exp", "Float64", null, ve);
 
         // Save as a matrix.
         let npcs = ve.length;
         let ncells = contents.pcs.length / npcs;
-        let pchandle = chandle.createDataSet("pcs", "Float64", [ncells, npcs]); // transposed in HDF5.
-        pchandle.write(contents.pcs);
+        chandle.writeDataSet("pcs", "Float64", [ncells, npcs], contents.pcs); // transposed in HDF5.
     }
 
     // Neighbor index.
@@ -213,8 +178,7 @@ export function convertFromVersion0(state, newfile) {
         let ghandle = fhandle.createGroup("neighbor_index");
         let phandle = ghandle.createGroup("parameters");
         let params = state.pca.parameters;
-        let dhandle = phandle.createDataSet("approximate", "Uint8", []);
-        dhandle.write(params.approximate);
+        phandle.writeDataSet("approximate", "Uint8", [], Number(params.approximate));
 
         ghandle.createGroup("results");
     }
@@ -225,29 +189,15 @@ export function convertFromVersion0(state, newfile) {
 
         let phandle = ghandle.createGroup("parameters");
         let params = state.tsne.parameters;
-        {
-            let dhandle = phandle.createDataSet("perplexity", "Float64", []);
-            dhandle.write(params.perplexity);
-        }
-        {
-            let dhandle = phandle.createDataSet("iterations", "Int32", []);
-            dhandle.write(params.iterations);
-        }
-        {
-            let dhandle = phandle.createDataSet("animate", "Uint8", []);
-            dhandle.write(params.animate);
-        }
+        phandle.writeDataSet("perplexity", "Float64", [], params.perplexity);
+        phandle.writeDataSet("iterations", "Int32", [], params.iterations);
+        phandle.writeDataSet("animate", "Uint8", [], params.animate);
 
         let chandle = ghandle.createGroup("results");
         let contents = recoverTypedArrays(state.tsne.contents);
-        {
-            let dhandle = chandle.createDataSet("x", "Float64", [contents.x.length]);
-            dhandle.write(contents.x);
-        }
-        {
-            let dhandle = chandle.createDataSet("y", "Float64", [contents.y.length]);
-            dhandle.write(contents.y);
-        }
+        chandle.writeDataSet("x", "Float64", null, contents.x);
+        chandle.writeDataSet("y", "Float64", null, contents.y);
+
         // Don't bother saving the number of iterations.
     }
 
@@ -257,33 +207,16 @@ export function convertFromVersion0(state, newfile) {
 
         let phandle = ghandle.createGroup("parameters");
         let params = state.umap.parameters;
-        {
-            let dhandle = phandle.createDataSet("num_neighbors", "Int32", []);
-            dhandle.write(params.num_neighbors);
-        }
-        {
-            let dhandle = phandle.createDataSet("num_epochs", "Int32", []);
-            dhandle.write(params.num_epochs);
-        }
-        {
-            let dhandle = phandle.createDataSet("min_dist", "Float64", []);
-            dhandle.write(params.min_dist);
-        }
-        {
-            let dhandle = phandle.createDataSet("animate", "Uint8", []);
-            dhandle.write(params.animate);
-        }
+        phandle.writeDataSet("num_neighbors", "Int32", [], params.num_neighbors);
+        phandle.writeDataSet("num_epochs", "Int32", [], params.num_epochs);
+        phandle.writeDataSet("min_dist", "Float64", [], params.min_dist);
+        phandle.writeDataSet("animate", "Uint8", [], Number(params.animate));
 
         let chandle = ghandle.createGroup("results");
         let contents = recoverTypedArrays(state.umap.contents);
-        {
-            let dhandle = chandle.createDataSet("x", "Float64", [contents.x.length]);
-            dhandle.write(contents.x);
-        }
-        {
-            let dhandle = chandle.createDataSet("y", "Float64", [contents.y.length]);
-            dhandle.write(contents.y);
-        }
+        chandle.writeDataSet("x", "Float64", null, contents.x);
+        chandle.writeDataSet("y", "Float64", null, contents.y);
+
         // Don't bother saving the number of iterations.
     }
 
@@ -303,9 +236,7 @@ export function convertFromVersion0(state, newfile) {
         let chandle = ghandle.createGroup("results");
         let contents = recoverTypedArrays(state.kmeans_cluster.contents);
         if ("kmeans_cluster" in state) {
-            let clusters = contents.clusters;
-            let dhandle = chandle.createDataSet("clusters", "Int32", [clusters.length]);
-            dhandle.write(clusters);
+            chandle.writeDataSet("clusters", "Int32", null, contents.clusters);
         }
     }
 
@@ -314,37 +245,25 @@ export function convertFromVersion0(state, newfile) {
         let ghandle = fhandle.createGroup("snn_graph_cluster");
 
         let phandle = ghandle.createGroup("parameters");
-        {
-            let find_params = state.snn_find_neighbors.parameters;
-            {
-                let dhandle = phandle.createDataSet("k", "Int32", []);
-                dhandle.write(find_params.k);
-            }
+        let find_params = state.snn_find_neighbors.parameters;
+        phandle.writeDataSet("k", "Int32", [], find_params.k);
 
-            let build_params = state.snn_build_graph.parameters;
-            quick_write_string(phandle, "scheme", ["rank", "number", "jaccard"][build_params.scheme]);
+        let build_params = state.snn_build_graph.parameters;
+        phandle.writeDataSet("scheme", "String", [], ["rank", "number", "jaccard"][build_params.scheme]);
 
-            let cluster_params = state.snn_cluster_graph.parameters;
-            {
-                let dhandle = phandle.createDataSet("resolution", "Float64", []);
-                dhandle.write(cluster_params.resolution);
-            }
-        }
+        let cluster_params = state.snn_cluster_graph.parameters;
+        phandle.writeDataSet("resolution", "Float64", [], cluster_params.resolution);
 
         let chandle = ghandle.createGroup("results");
         let contents = recoverTypedArrays(state.snn_cluster_graph.contents);
-        {
-            let clusters = contents.clusters;
-            let dhandle = chandle.createDataSet("clusters", "Int32", [clusters.length]);
-            dhandle.write(clusters);
-        }
+        chandle.writeDataSet("clusters", "Int32", null, contents.clusters);
     }
 
     // Choose clustering.
     {
         let ghandle = fhandle.createGroup("choose_clustering");
         let phandle = ghandle.createGroup("parameters");
-        quick_write_string(phandle, "method", state.choose_clustering.parameters.method);
+        phandle.writeDataSet("method", "String", [], state.choose_clustering.parameters.method);
     }
 
     // Marker detection.
@@ -360,9 +279,7 @@ export function convertFromVersion0(state, newfile) {
             let current = recoverTypedArrays(val);
 
             for (const x of [ "means", "detected" ]) {
-                let y = current[x];
-                let dhandle = ihandle.createDataSet(x, "Float64", [y.length]);
-                dhandle.write(y);
+                ihandle.writeDataSet(x, "Float64", null, current[x]);
             }
 
             for (const i of [ "lfc", "delta_detected", "auc", "cohen" ]) {
@@ -370,10 +287,8 @@ export function convertFromVersion0(state, newfile) {
                 let rhandle = ihandle.createGroup(i);
 
                 for (const j of [ "min", "mean", "min-rank" ]) {
-                    let y = rankings[j];
                     let name = (j == "min-rank" ? "min_rank" : j);
-                    let dhandle = rhandle.createDataSet(name, "Float64", [y.length]);
-                    dhandle.write(y);
+                    rhandle.writeDataSet(name, "Float64", null, rankings[j]);
                 }
             }
         }
@@ -387,8 +302,7 @@ export function convertFromVersion0(state, newfile) {
         let shandle = phandle.createGroup("selections");
         let params = state.custom_marker_management.parameters;
         for (const [key, val] of Object.entries(params.selections)) {
-            let dhandle = shandle.createDataSet(String(key), "Int32", [val.length]);
-            dhandle.write(val);
+            shandle.writeDataSet(String(key), "Int32", null, val);
         }
 
         let chandle = ghandle.createGroup("results");
@@ -398,15 +312,11 @@ export function convertFromVersion0(state, newfile) {
             let current = recoverTypedArrays(val);
 
             for (const x of [ "means", "detected" ]) {
-                let y = current[x];
-                let dhandle = ihandle.createDataSet(x, "Float64", [y.length]);
-                dhandle.write(y);
+                ihandle.writeDataSet(x, "Float64", null, current[x]);
             }
 
             for (const i of [ "lfc", "delta_detected", "auc", "cohen" ]) {
-                let y = current[i]["mean"];
-                let dhandle = ihandle.createDataSet(i, "Float64", [y.length]);
-                dhandle.write(y);
+                ihandle.writeDataSet(i, "Float64", null, current[i]["mean"]);
             }
         }
     }
