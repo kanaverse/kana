e865f2848bc3d2a58a710c9e32b0c33ee0ffe1a9
jkanche <jayaram.kancherla@gmail.com>
2022-05-10 15:10:41 -0700

    more css magic for drag and drop

--- src/components/Gallery/SortableItem.js
+++ src/components/Gallery/SortableItem.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import {useSortable} from '@dnd-kit/sortable';
-import {CSS} from '@dnd-kit/utilities';
+import React from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { Button, Divider, Icon } from "@blueprintjs/core";
+
+import "./Gallery.css";
 
 export function SortableItem(props) {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-  } = useSortable({id: props.id});
-  
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: props.id });
+
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
   };
-  
+
+  // TODO: Template for each item in the Gallery
+  // <div className="gallery-cont">
+  // <div className={props?.showQCLoader ? "gitem effect-opacitygrayscale" : "gitem"}>
+  //   <div className="gitem-header">
+  //     <div className="gitem-header-title">An example panel</div>
+  //     <div className="gitem-header-actions">
+  //       <Button icon="download" small={true} />
+  //       <Button icon="trash" small={true} />
+  //       <Button icon="select" small={true} />
+  //     </div>
+  //   </div>
+  //   <Divider />
+  //   <div className="gitem-content"></div>
+  // </div>
+
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      {props.content}
+    <div ref={setNodeRef} style={style} key={props?.id} className={"gitem"}>
+      <div className="gitem-header" {...attributes} {...listeners}>
+        {/* <Icon icon="move" /> */}
+        <div className="gitem-header-title">{props?.title}</div>
+        <div className="gitem-header-actions">
+          {props?.actions?.map((x) => (
+            <Button icon={x} small={true} />
+          ))}
+        </div>
+      </div>
+      <Divider />
+      <div className="gitem-content">{props?.content}</div>
     </div>
   );
 }
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -51,21 +51,6 @@ const Gallery = (props) => {
     return text;
   }
 
-  // TODO: Template for each item in the Gallery
-  // <div className="gallery-cont">
-  // <div className={props?.showQCLoader ? "gitem effect-opacitygrayscale" : "gitem"}>
-  //   <div className="gitem-header">
-  //     <div className="gitem-header-title">An example panel</div>
-  //     <div className="gitem-header-actions">
-  //       <Button icon="download" small={true} />
-  //       <Button icon="trash" small={true} />
-  //       <Button icon="select" small={true} />
-  //     </div>
-  //   </div>
-  //   <Divider />
-  //   <div className="gitem-content"></div>
-  // </div>
-
   useEffect(() => {
     get_children();
   }, [props]);
@@ -84,28 +69,13 @@ const Gallery = (props) => {
 
         tmpItems.push("1");
         tmpItemContent["1"] = {
-          id: 1,
-          content: (
-            <div
-              key={`qc-${x}`}
-              className={
-                props?.showQCLoader ? "gitem effect-opacitygrayscale" : "gitem"
-              }
-            >
-              <div className="gitem-header">
-                <div className="gitem-header-title">QC for {x}</div>
-                <div className="gitem-header-actions">
-                  <Button icon="download" small={true} />
-                </div>
-              </div>
-              <Divider />
-              <div className="gitem-content">
-                {/* <Card key={x} className={props?.showQCLoader ? "gallery-elem effect-opacitygrayscale" : "gallery-elem"} elevation={Elevation.ONE}> */}
-                <QCPlotMgr title={x} data={tqc} />
-                {/* </Card> */}
-              </div>
-            </div>
-          ),
+          // id: 1,
+          title: `QC for ${x}`,
+          className: props?.showQCLoader
+            ? "gitem effect-opacitygrayscale"
+            : "gitem",
+          actions: ["download"],
+          content: <QCPlotMgr title={x} data={tqc} />,
         };
       });
     }
@@ -114,66 +84,35 @@ const Gallery = (props) => {
       tmpItems.push("2");
 
       tmpItemContent["2"] = {
-        id: 2,
+        // id: 2,
+        title: "PCA: % variance explained",
+        className: props?.showPCALoader
+          ? "gitem effect-opacitygrayscale"
+          : "gitem",
+        actions: ["download"],
         content: (
-          <div
-            key={`pca`}
-            className={
-              props?.showPCALoader ? "gitem effect-opacitygrayscale" : "gitem"
-            }
-          >
-            <div className="gitem-header">
-              <div className="gitem-header-title">
-                PCA: % variance explained
-              </div>
-              <div className="gitem-header-actions">
-                <Button icon="download" small={true} />
-              </div>
-            </div>
-            <Divider />
-            <div className="gitem-content">
-              {/* <Card className={props?.showPCALoader ? "gallery-elem effect-opacitygrayscale" : "gallery-elem"} elevation={Elevation.ONE}> */}
-              <PCABarPlot
-                title={datasetName.split(" ").join("_")}
-                pca={props?.pcaVarExp}
-              />
-              {/* </Card> */}
-            </div>
-          </div>
+          <PCABarPlot
+            title={datasetName.split(" ").join("_")}
+            pca={props?.pcaVarExp}
+          />
         ),
       };
     }
 
     if (props?.clusterData && props?.clusterColors) {
       tmpItems.push("3");
-
       tmpItemContent["3"] = {
-        id: 3,
+        // id: 3,
+        title: "Cluster: Num. of cells per cluster",
+        className: props?.showNClusLoader
+          ? "gitem effect-opacitygrayscale"
+          : "gitem",
+        actions: ["download"],
         content: (
-          <div
-            key={`clus`}
-            className={
-              props?.showNClusLoader ? "gitem effect-opacitygrayscale" : "gitem"
-            }
-          >
-            <div className="gitem-header">
-              <div className="gitem-header-title">
-                Cluster: Num. of cells per cluster
-              </div>
-              <div className="gitem-header-actions">
-                <Button icon="download" small={true} />
-              </div>
-            </div>
-            <Divider />
-            <div className="gitem-content">
-              {/* <Card className={props?.showNClusLoader ? "gallery-elem effect-opacitygrayscale" : "gallery-elem"} elevation={Elevation.ONE}> */}
-              <ClusterBarPlot
-                data={props?.clusterData}
-                clusterColors={props?.clusterColors}
-              />
-              {/* </Card> */}
-            </div>
-          </div>
+          <ClusterBarPlot
+            data={props?.clusterData}
+            clusterColors={props?.clusterColors}
+          />
         ),
       };
     }
@@ -183,97 +122,81 @@ const Gallery = (props) => {
       Object.keys(props?.cellLabelData?.per_reference).length > 0
     ) {
       tmpItems.push("4");
-
       tmpItemContent["4"] = {
-        id: 4,
-        content: (
-          <div
-            key={`celllabel`}
-            className={
-              props?.showCellLabelLoader
-                ? "gitem effect-opacitygrayscale"
-                : "gitem"
-            }
-          >
-            <div className="gitem-header">
-              <div className="gitem-header-title">
-                <Popover2
-                  popoverClassName={Classes.POPOVER_CONTENT_SIZING}
-                  hasBackdrop={false}
-                  interactionKind="hover"
-                  placement="auto"
-                  hoverOpenDelay={50}
-                  modifiers={{
-                    arrow: { enabled: true },
-                    flip: { enabled: true },
-                    preventOverflow: { enabled: true },
-                  }}
-                  content={
-                    <Card
-                      style={{
-                        width: "450px",
-                      }}
-                      elevation={Elevation.ZERO}
-                    >
-                      <p>
-                        Perform cell type annotation for human and mouse
-                        datasets. This uses the{" "}
-                        <a
-                          target="_blank"
-                          href="https://bioconductor.org/packages/release/bioc/html/SingleR.html"
-                        >
-                          SingleR
-                        </a>{" "}
-                        algorithm to label clusters based on their similarity to
-                        reference expression profiles of curated cell types.
-                        Similarity is quantified using Spearman correlations on
-                        the top marker genes for each reference type, with
-                        additional fine-tuning iterations to improve resolution
-                        between closely related labels.
-                      </p>
-                      <p>
-                        <strong>Best match</strong>: Classification of the
-                        clusters is performed separately for each chosen
-                        reference reference. If multiple references are
-                        selected, an additional round of scoring is performed to
-                        determine which reference has the best label for each
-                        cluster.
-                      </p>
-                    </Card>
-                  }
-                >
-                  <span
-                    style={{
-                      textDecoration: "underline",
-                      cursor: "help",
-                    }}
-                  >
-                    Cell Labels
-                  </span>
-                </Popover2>
-                <span
+        // id: 4,
+        title: (
+          <>
+            {" "}
+            <Popover2
+              popoverClassName={Classes.POPOVER_CONTENT_SIZING}
+              hasBackdrop={false}
+              interactionKind="hover"
+              placement="auto"
+              hoverOpenDelay={50}
+              modifiers={{
+                arrow: { enabled: true },
+                flip: { enabled: true },
+                preventOverflow: { enabled: true },
+              }}
+              content={
+                <Card
                   style={{
-                    fontStyle: "italic",
-                    color: "#2B95D6",
-                    fontWeight: "bold",
+                    width: "450px",
                   }}
+                  elevation={Elevation.ZERO}
                 >
-                  {" "}
-                  (best match)
-                </span>
-              </div>
-              <div className="gitem-header-actions">
-                <Button icon="download" small={true} />
-              </div>
-            </div>
-            <Divider />
-            <div className="gitem-content">
-              {/* <Card className={props?.showCellLabelLoader ? "gallery-elem effect-opacitygrayscale" : "gallery-elem"} elevation={Elevation.ONE}> */}
-              <CellLabelTable data={props?.cellLabelData} />
-              {/* </Card> */}
-            </div>
-          </div>
+                  <p>
+                    Perform cell type annotation for human and mouse datasets.
+                    This uses the{" "}
+                    <a
+                      target="_blank"
+                      href="https://bioconductor.org/packages/release/bioc/html/SingleR.html"
+                    >
+                      SingleR
+                    </a>{" "}
+                    algorithm to label clusters based on their similarity to
+                    reference expression profiles of curated cell types.
+                    Similarity is quantified using Spearman correlations on the
+                    top marker genes for each reference type, with additional
+                    fine-tuning iterations to improve resolution between closely
+                    related labels.
+                  </p>
+                  <p>
+                    <strong>Best match</strong>: Classification of the clusters
+                    is performed separately for each chosen reference reference.
+                    If multiple references are selected, an additional round of
+                    scoring is performed to determine which reference has the
+                    best label for each cluster.
+                  </p>
+                </Card>
+              }
+            >
+              <span
+                style={{
+                  textDecoration: "underline",
+                  cursor: "help",
+                }}
+              >
+                Cell Labels
+              </span>
+            </Popover2>
+            <span
+              style={{
+                fontStyle: "italic",
+                color: "#2B95D6",
+                fontWeight: "bold",
+              }}
+            >
+              {" "}
+              (best match)
+            </span>
+          </>
         ),
+        className: props?.showCellLabelLoader
+          ? "gitem effect-opacitygrayscale"
+          : "gitem",
+        actions: ["download"],
+        content: <CellLabelTable data={props?.cellLabelData} />,
       };
     }
 
@@ -281,61 +204,37 @@ const Gallery = (props) => {
       props?.savedPlot.map((x, i) => {
         tmpItems.push(`${5 + i}`);
         tmpItemContent[`${5 + i}`] = {
-          id: 5 + i,
-          content: (
-            <div key={"img-" + i} className={"gitem"}>
-              <div className="gitem-header">
-                <div className="gitem-header-title">{get_image_title(x)}</div>
-                <div className="gitem-header-actions">
-                  <Button icon="select" small={true} />
-                  <Button icon="download" small={true} />
-                  <Button icon="trash" small={true} />
-                </div>
-              </div>
-              <Divider />
-              <div className="gitem-content">
-                {/* <Card key={i} className="gallery-elem" elevation={Elevation.ONE}> */}
-                <ImgPlot data={x} gene={props?.gene} />
-                {/* </Card> */}
-              </div>
-            </div>
-          ),
+          // id: 5 + i,
+          title: get_image_title(x),
+          className: "gitem",
+          actions: ["select", "download", "trash"],
+          content: <ImgPlot data={x} gene={props?.gene} />,
         };
       });
     }
 
     tmpItems.push(`${12}`);
     tmpItemContent[`${12}`] = {
-      id: 12,
+      // id: 12,
+      title: "UMAP",
+      className: "gitem",
+      actions: ["select", "download", "trash"],
       content: (
-        <div key={"img-" + 12} className={"gitem"}>
-          <div className="gitem-header">
-            <div className="gitem-header-title">{"UMAP"}</div>
-            <div className="gitem-header-actions">
-              <Button icon="select" small={true} />
-              <Button icon="download" small={true} />
-              <Button icon="trash" small={true} />
-            </div>
-          </div>
-          <Divider />
-          <div className="gitem-content">
-            <UDimPlot
-              tsneData={props?.tsneData} 
-              umapData={props?.umapData}
-              selectedPoints={props?.selectedPoints}
-              setSelectedPoints={props?.setSelectedPoints}
-              data={{
-                color: "#fff000",
-                config: {
-                  embedding: "UMAP",
-                  annotation: null,
-                  highlight: null,
-                  gene: null,
-                },
-              }}
-            />
-          </div>
-        </div>
+        <UDimPlot
+          tsneData={props?.tsneData}
+          umapData={props?.umapData}
+          selectedPoints={props?.selectedPoints}
+          setSelectedPoints={props?.setSelectedPoints}
+          data={{
+            color: "#fff000",
+            config: {
+              embedding: "UMAP",
+              annotation: null,
+              highlight: null,
+              gene: null,
+            },
+          }}
+        />
       ),
     };
 
@@ -370,7 +269,7 @@ const Gallery = (props) => {
     <DndContext modifiers={[restrictToWindowEdges]} onDragEnd={handleDragEnd}>
       <SortableContext items={items} strategy={verticalListSortingStrategy}>
         {items.map((x) => (
-          <SortableItem key={x} id={x} content={itemContent[x]?.content} />
+          <SortableItem key={x} id={x} {...itemContent[x]}/>
         ))}
       </SortableContext>
     </DndContext>
