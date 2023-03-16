4915ade8b9fad116cb281b830bab075daf9c470b
jkanche <jayaram.kancherla@gmail.com>
2023-03-15 19:53:01 -0700

    fix missing stats header

--- src/components/Stats/index.js
+++ src/components/Stats/index.js
@@ -1,14 +1,11 @@
 const Stats = (props) => {
-
-    return (
-        <>
-            {
-                <span>
-                    {props?.headerInfo}
-                </span>
-            }
-        </>
-    );
+  return (
+    <span>
+      {props?.initDims ? `: ${props?.initDims}` : " "}
+      {props?.qcDims ? ` (${props?.qcDims} after QC)` : ""}
+      {props?.headerInfo}
+    </span>
+  );
 };
 
 export default Stats;
