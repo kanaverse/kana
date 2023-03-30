a53b4a8fa5579f6a865343c0da8c793f91c1ee8f
jkanche <jayaram.kancherla@gmail.com>
2023-03-29 20:58:02 -0700

    cleaning up the landing page even more, we can always revert if you don't like this layout

--- src/App.js
+++ src/App.js
@@ -56,47 +56,58 @@ function App() {
             </NavbarGroup>
           </Navbar>
           <div className="frontpage">
-            <div className="frontpage-row">
-              <h1>Analyze single-cell datasets in the browser</h1>
+            <div className="frontpage-logo">
+              <img
+                src={gif}
+                style={{ border: "5px solid #eeeeee" }}
+                width={245}
+                height={245}
+              />
             </div>
-            <div className="frontpage-row">
+            <div className="frontpage-content">
+              <H2>Analyze single-cell datasets in the browser</H2>
               <p>
-                <strong>kana</strong> uses WebAssembly and other technologies to efficiently analyze large single-cell datasets in the browser.
+                <strong>kana</strong> uses WebAssembly and other technologies to
+                efficiently analyze large single-cell datasets in the browser.
               </p>
-              <br/>
               <ul>
                 <li>Always free. It's your analysis running on your device.</li>
-                <li>Always available. Because science can't wait for server outages.</li> 
+                <li>
+                  Always available. Because science can't wait for server
+                  outages.
+                </li>
                 <li>Always private. Data never, ever leaves your computer.</li>
               </ul>
-              <br/>
               <p>
-              Check out our{" "}
-              <a href="https://github.com/kanaverse" target="_blank">
-                GitHub page
-              </a>{" "}
-              or our{" "}
-              <a href="https://doi.org/10.1101/2022.03.02.482701" target="_blank">
-                bioRxiv manuscript
-              </a>{" "}
-              for more details.
+                Check out our{" "}
+                <a href="https://github.com/kanaverse" target="_blank">
+                  GitHub page
+                </a>{" "}
+                or our{" "}
+                <a
+                  href="https://doi.org/10.1101/2022.03.02.482701"
+                  target="_blank"
+                >
+                  bioRxiv manuscript
+                </a>{" "}
+                for more details.
               </p>
             </div>
-            <div className="frontpage-row">
+            <div className="frontpage-actions">
               <Callout
-                title="I want to analyze a new dataset" 
+                title="I want to analyze a new dataset"
                 onClick={() => setAppMode("analysis")}
                 className="frontpage-rowitem"
                 icon="function"
                 intent="primary"
               >
                 <p>
-                  Provide a single-cell dataset in one of the accepted formats and <strong>kana</strong> will perform a standard single-cell data analysis.
-                  Get your UMAPs, t-SNEs, clusters and marker genes with a click of a button. 
+                  Provide a single-cell dataset in one of the accepted formats
+                  and <strong>kana</strong> will perform a standard single-cell
+                  data analysis. Get your UMAPs, t-SNEs, clusters and marker
+                  genes with a click of a button.
                 </p>
               </Callout>
-            </div>
-            <div className="frontpage-row">
               <Callout
                 title="I want to explore existing analysis results"
                 onClick={() => setAppMode("explore")}
@@ -105,15 +116,12 @@ function App() {
                 intent="success"
               >
                 <p>
-                  Provide a pre-analyzed single-cell dataset in one of the accepted formats and <strong>kana</strong> will load the existing results into the browser for further exploration.
+                  Provide a pre-analyzed single-cell dataset in one of the
+                  accepted formats and <strong>kana</strong> will load the
+                  existing results into the browser for further exploration.
                 </p>
               </Callout>
             </div>
-            {appMode === null && 
-                <div className="frontpage-row">
-                <img src={gif} style={{border: "5px solid #eeeeee"}}/>
-                </div>
-            }
           </div>
         </div>
       )}
