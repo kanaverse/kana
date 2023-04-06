061662e59548ddf72f6d39bbc9cb87e9360b9a46
jkanche <jayaram.kancherla@gmail.com>
2023-04-05 20:13:30 -0700

    fix issue when there are batches for qc results

--- src/workers/helpers.js
+++ src/workers/helpers.js
@@ -57,11 +57,11 @@ export function postError(type, err, fatal) {
 
 export function splitMetricsByBlock(metrics, blockLevels, blockIds) {
   var output = {};
-  var blocks = blockIds.array();
+  var blocks = blockIds.slice();
   for (var b = 0; b < blockLevels.length; b++) {
     let current = {};
     for (const [key, val] of Object.entries(metrics)) {
-      current[key] = val.array().filter((x, i) => blocks[i] == b);
+      current[key] = val.slice().filter((x, i) => blocks[i] == b);
     }
     output[blockLevels[b]] = current;
   }
